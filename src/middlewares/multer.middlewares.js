7f277f77561ba8e4358327f8d7d2df3ecba89281
Ajmatraj <159477674+Ajmatraj@users.noreply.github.com>
2024-02-27 19:16:01 +0545

    env file

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
       cb(null, "./public/temp")
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Generate a unique filename
+      cb(null, file.originalname); // Generate a unique filename
     }
   })
   
